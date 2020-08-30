52d31c9aee32151c3b90952cb77bc9830348d6c7
whyimnobody <seven@whyimnobody.ltd>
2020-08-30 09:53:55 +0200

    feat(test): MARS-2_Expand tests and leave this alone - Abstract command execution to make testing easier - Add tests to ensure command execution works - Add tests aware of plateau edges

--- rover.py
+++ rover.py
@@ -2,59 +2,73 @@ from collections import deque
 from sys import exit
 
 
+max_x, max_y = 10, 10
+
+
 class Rover:
     """Object-representation of a Rover
 
-    Each rover can be represented as an object with the position and orientation
-    as properties
-    """
+	Each rover can be represented as an object with the position and orientation
+	as properties
+	"""
+
+    POSSIBLE_ORIENTATIONS = ["N", "E", "S", "W"]  # The compass points as a list
 
     def __init__(self, x_position, y_position, orientation):
         """
-        @param x_position: The rover's x-position
-        @param y_position: The rover's y-position
-        @param orientation: The direction the rover is facing
-        """
-
+		@param x_position: The rover's x-position
+		@param y_position: The rover's y-position
+		@param orientation: The direction the rover is facing
+		"""
+        # TODO(ksebopelo): Add check to ensure starting position is in plateau
+        # TODO(ksebopelo): Add check to ensure orientation is valid
+        # TODO(ksebopelo): Add check to ensure rovers don't collide (start)
         self.x_position = x_position
         self.y_position = y_position
-        orientation_index = POSSIBLE_ORIENTATIONS.index(orientation.upper())
-        self.orientation = deque(POSSIBLE_ORIENTATIONS)
-        self.orientation.rotate(-orientation_index)
+        orientation_index = self.POSSIBLE_ORIENTATIONS.index(orientation.upper())
+        self._orientation = deque(self.POSSIBLE_ORIENTATIONS)
+        self._orientation.rotate(-orientation_index)
+
+    @property
+    def orientation(self):
+        return self._orientation[0]
 
     def rotate(self, direction):
         """Rotate the rover in-place
-        @param direction: The direction in which the rover should rotate
-        """
+		@param direction: The direction in which the rover should rotate
+		"""
         if direction.upper() == "L":
-            self.orientation.rotate(1)
+            self._orientation.rotate(1)
         elif direction.upper() == "R":
-            self.orientation.rotate(-1)
+            self._orientation.rotate(-1)
         else:
             print("Not a valid direction in which to rotate")
 
     def move(self):
         """Move the rover forward
-        """
-        if self.orientation[0] == "N":
+		"""
+        # TODO(ksebopelo): Add check to ensure rovers don't collide (moving)
+        if self.orientation == "N":
             self.y_position = (
                 self.y_position + 1 if self.y_position < max_x else self.y_position
             )
-        elif self.orientation[0] == "E":
+        elif self.orientation == "E":
             self.x_position = (
                 self.x_position + 1 if self.x_position < max_y else self.x_position
             )
-        elif self.orientation[0] == "S":
+        elif self.orientation == "S":
             self.y_position = (
                 self.y_position - 1 if self.y_position > 0 else self.y_position
             )
-        elif self.orientation[0] == "W":
+        elif self.orientation == "W":
             self.x_position = (
                 self.x_position - 1 if self.x_position > 0 else self.x_position
             )
 
     def parse_commands(self, commands):
-        """Parse the alternating instructions from input to rotate or move accordingly"""
+        """Parse the alternating instructions from input to rotate or move
+		accordingly
+		"""
         for cmd in commands:
             if cmd.upper() == "M":
                 self.move()
@@ -62,22 +76,18 @@ class Rover:
                 self.rotate(cmd)
 
 
-POSSIBLE_ORIENTATIONS = ["N", "E", "S", "W"]  # The compass points as a list
-max_x, max_y = 0, 0
-
-
 def set_plateau_limits(coordinates):
     """Set the maximum x and maximum y coordinates
-    @param coordinates: A string indicating the top right position of the plateau and setting the variables as specified
-    """
+	@param coordinates: A string indicating the top right position of the plateau and setting the variables as specified
+	"""
     coordinates = coordinates.split()
     return list(map(int, coordinates))
 
 
 def prep_initial_rover_details(initial_points):
     """Return the initial coordinates as integers and orientation as an uppercase string
-    @param initial_points: String containing initial points and orientations
-    """
+	@param initial_points: String containing initial points and orientations
+	"""
     initial_input = initial_points.split()
     orientation = initial_input.pop()
     x_position, y_position = list(map(int, initial_input))
@@ -85,8 +95,27 @@ def prep_initial_rover_details(initial_points):
     return x_position, y_position, orientation
 
 
+def execute_rover_commands(commands):
+    """Abstraction of command execution for testing
+	@param commands: List of commands as tendered for rover details
+	"""
+    rovers = []
+
+    for each in range(len(commands) // 2):
+        rover_coordinates = commands.pop(0)
+        rover_movements = commands.pop(0)
+        # Prepare the object with its position and orientation
+        rover_input = prep_initial_rover_details(rover_coordinates)
+        rover = Rover(*rover_input)
+        # Move the rover and add the object to the rovers list!
+        rover.parse_commands(rover_movements)
+        rovers.append(rover)
+
+    return rovers
+
+
 def main_rover_application():
-    """Provide a loop for user to input rover inputs"""
+    """Provide a loop for user to input rover commands"""
 
     complete = False
     instructions = []
@@ -109,15 +138,7 @@ def main_rover_application():
     max_x, max_y = set_plateau_limits(plateau_limits)
 
     # Set each rovers position, and move it accordingly
-    for each in range(len(instructions) // 2):
-        rover_coordinates = instructions.pop(0)
-        rover_movements = instructions.pop(0)
-        # Prepare the object with its position and orientation
-        rover_input = prep_initial_rover_details(rover_coordinates)
-        rover = Rover(*rover_input)
-        # Move the rover and add the object to the rovers list!
-        rover.parse_commands(rover_movements)
-        rovers.append(rover)
+    rovers = execute_rover_commands(instructions)
 
     # Display final rover position and orientation for each rover
     for rover in rovers:
--- test_rover.py
+++ test_rover.py
@@ -3,7 +3,7 @@ import random
 import pytest
 
 from rover import (
-    POSSIBLE_ORIENTATIONS,
+    execute_rover_commands,
     prep_initial_rover_details,
     Rover,
     set_plateau_limits,
@@ -19,6 +19,46 @@ def given_rover_input():
         "3 3 E",
         "MMRMMRMRRM",
     ]
+
+
+@pytest.fixture
+def given_rover_commands():
+    return [
+        "3 3 E",
+        "MMRMMRMRRM",
+    ]
+
+
+def test_rover_rotate_left():
+    rover = Rover(1, 2, "N")
+    rover.rotate("L")
+    assert rover.orientation == "W"
+
+
+def test_rover_rotate_right():
+    rover = Rover(4, 5, "S")
+    rover.rotate("R")
+    assert rover.orientation == "W"
+
+
+def test_rover_move_forward_in_plateau():
+    rover = Rover(1, 2, "S")
+    rover.move()
+    # NOTE: Garbage test, due to the way max_x and max_y are defined in script
+    assert rover.y_position == 1
+
+
+def test_rover_move_forward_edge_plateau():
+    rover = Rover(0, 10, "W")
+    rover.move()
+    # NOTE: Garbage test, due to the way max_x and max_y are defined in script
+    assert rover.x_position == 0
+
+
+def test_rover_executes_commands(given_rover_commands):
+    rovers = execute_rover_commands(given_rover_commands)
+    rover = rovers[0]
+    assert rover.__dict__ == Rover(5, 1, "E").__dict__
 
 
 def test_prep_initial_rover_details():
